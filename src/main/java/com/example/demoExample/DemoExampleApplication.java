3f13b29023eb5d736a9ff12a6348908885ecc564
rolquism117 <rquispe@avatar-global.com>
2024-05-23 15:37:51 -0500

    Spring + Docker Funcional

--- src/main/java/com/example/demoExample/DemoExampleApplication.java
+++ src/main/java/com/example/demoExample/DemoExampleApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoExampleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoExampleApplication.class, args);
+    }
 }
--- src/main/java/com/example/demoExample/controller/HelloController.java
+++ src/main/java/com/example/demoExample/controller/HelloController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HolaMundoController {
-
-    @GetMapping("/hola")
-    public String holaMundo() {
-        return "Hola Mundo";
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
     }
 }
