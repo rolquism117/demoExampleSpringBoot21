0353e4db8f2472ef8ec5195866af9748047d18ca
rolquism117 <rquispe@avatar-global.com>
2024-05-27 15:50:16 -0500

    RUN SOAP UI - STABLE

--- src/main/java/com/example/demoExample/config/WebServiceConfig.java
+++ src/main/java/com/example/demoExample/config/WebServiceConfig.java
@@ -1,32 +1,37 @@
 package com.example.demoExample.config;
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
-import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
-import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.xml.xsd.XsdSchema;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 
 @EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
+
     @Bean
-    public MessageDispatcherServlet messageDispatcherServlet() {
-        return new MessageDispatcherServlet();
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
     }
 
     @Bean(name = "hello")
-    public Wsdl11Definition helloWsdl11Definition(XsdSchema helloSchema) {
-        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-        wsdl11Definition.setPortTypeName("HelloPortType");
-        wsdl11Definition.setLocationUri("/soap-api");
-        wsdl11Definition.setTargetNamespace("http://example.com/soap-web-service");
-        wsdl11Definition.setSchema(helloSchema);
-        return wsdl11Definition;
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema helloSchema) {
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+        definition.setPortTypeName("HelloPort");
+        definition.setLocationUri("/ws");
+        definition.setTargetNamespace("http://example.com/soap-web-service");
+        definition.setSchema(helloSchema);
+        return definition;
     }
 
     @Bean
--- src/main/java/com/example/soap_web_service/package-info.java
+++ src/main/java/com/example/soap_web_service/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.05.27 a las 02:11:36 PM PET 
+// Generado el: 2024.05.27 a las 03:42:04 PM PET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://example.com/soap-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
