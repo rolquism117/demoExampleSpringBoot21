ba03f5c1143230449b38ade6b9811176d5f1ac98
rolquism117 <rquispe@avatar-global.com>
2024-05-28 20:50:51 -0500

    ERROR 500 - SOAP UI

--- src/main/java/com/example/demoExample/endpoint/HelloEndpoint.java
+++ src/main/java/com/example/demoExample/endpoint/HelloEndpoint.java
@@ -2,24 +2,19 @@ package com.example.demoExample.endpoint;
 
 import com.example.soap_web_service.GetHelloRequest;
 import com.example.soap_web_service.GetHelloResponse;
-import org.springframework.stereotype.Component;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
-@Component
 public class HelloEndpoint {
-    private static final String NAMESPACE_URI = "http://example.com/soap-web-service";
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetHelloRequest")
+    @PayloadRoot(namespace = "http://example.com/soap-web-service", localPart = "GetHelloRequest")
     @ResponsePayload
     public GetHelloResponse getHello(@RequestPayload GetHelloRequest request) {
         GetHelloResponse response = new GetHelloResponse();
-        String name = request.getName();
-        String greeting = "Hello, " + name + "!";
-        response.setGreeting(greeting);
+        response.setGreeting("Hello, " + request.getName());
         return response;
     }
 }
